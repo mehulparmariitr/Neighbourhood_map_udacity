6b0ad3691b5d598e92ba6632670eb95b7c0561b9
Mehul <mehulparmariitr@gmail.com>
2018-09-21 00:19:44 +0530

    added app.js

---
+++ js/app.js
@@ -0,0 +1,39 @@
+/*
+ Foursquare API Constants.
+ */
+var SEARCH_LIMIT = 20;
+var CLIENT_SECRET = 'LEF3BO5TX2WFHKXCPE4URBDMFEIUM1XWOJZFEWNZGMIYZRD5';
+var CLIENT_ID = 'SDNP0JJPPKBKKG11KRSF13ROYLCFMV21UFO0V0EOQV3EZYGN';
+var FSQ_URL = 'https://api.foursquare.com/v2/venues/search?&client_id=' +
+    CLIENT_ID + '&client_secret=' + CLIENT_SECRET +
+    '&near=Pune&limit=' + SEARCH_LIMIT;
+
+
+
+
+/*
+ * @description Error handler for loading of Google Maps APIs.
+ * */
+function mapLoadingError() {
+    alertify.error('Failed to load Google Maps <br /> Please check your internet connection.');
+}
+
+
+/*
+ * @description The Starting Point
+ */
+function startApp() {
+    //viewModel = new AppViewModel();
+    //ko.applyBindings(viewModel);
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 30
+    });
+
+    //infoWindow = new google.maps.InfoWindow();
+    //viewModel.loadLocations(FSQ_REQUEST_URL);
+    // Subscribe to filteredLocations, change Map Items when it's changed.
+    //viewModel.filteredLocations.subscribe(function () {
+    //editMap();
+    //});
+}
