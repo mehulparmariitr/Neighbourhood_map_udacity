e277380a02f220c6ce51324ddf183b3872c6f241
Mehul Kumar Parmar <MParmar@slb.com>
2018-09-24 10:33:42 +0530

    added bouncing of marker

--- js/app.js
+++ js/app.js
@@ -14,11 +14,8 @@ var FSQ_URL =
 
 var map = null;
 
-
-
-
 //model for the program
-var Model = function (data) {
+var Model = function(data) {
   var self = this;
 
   this.name = data.name;
@@ -29,7 +26,7 @@ var Model = function (data) {
   this.country = data.country;
   this.url = data.url;
 
-  this.address = ko.pureComputed(function () {
+  this.address = ko.pureComputed(function() {
     if ((self.loc && self.state && self.country) !== undefined)
       return self.loc + ", " + self.state + ", " + self.country;
     else return "No address found";
@@ -40,27 +37,23 @@ var Model = function (data) {
   this.infoWindowContent = data.infoWindowContent;
 };
 
-
-
-
 // ViewModel for the program
-var viewModel = function (data) {
+var viewModel = function(data) {
   var self = this;
 
   this.locations = ko.observableArray();
 
   this.hamburgerMenuShown = ko.observable(false);
-  this.showHamburgerMenu = function () {
+  this.showHamburgerMenu = function() {
     this.hamburgerMenuShown(!this.hamburgerMenuShown());
   };
-  this.filterText = ko.observable('');
+  this.filterText = ko.observable("");
 
-  this.getLocations = function (url) {
-    $.getJSON(url, function (data) {
-
+  this.getLocations = function(url) {
+    $.getJSON(url, function(data) {
       var venues = data.response.venues;
 
-      venues.forEach(function (venue) {
+      venues.forEach(function(venue) {
         var marker = new google.maps.Marker({
           map: null,
           animation: google.maps.Animation.DROP,
@@ -68,14 +61,14 @@ var viewModel = function (data) {
           title: venue.name
         });
         var ModelVenue = new Model({
-          'state': venue.location.state,
-          'country': venue.location.country,
-          'url': venue.url,
-          'marker': marker,
-          'name': venue.name,
-          'lat': venue.location.lat,
-          'lng': venue.location.lng,
-          'loc': venue.location.address
+          state: venue.location.state,
+          country: venue.location.country,
+          url: venue.url,
+          marker: marker,
+          name: venue.name,
+          lat: venue.location.lat,
+          lng: venue.location.lng,
+          loc: venue.location.address
         });
         ModelVenue.infoWindowContent = createInfoWindow(ModelVenue);
 
@@ -84,16 +77,19 @@ var viewModel = function (data) {
 
       // Re-Load the markers on the map once the data arrives.
       setupMarkers();
-    }).fail(function (jqXHR, textStatus, error) {
-      alertify.error('Error in getting data <br />' + error);
+    }).fail(function(jqXHR, textStatus, error) {
+      alertify.error("Error in getting data <br />" + error);
     });
   };
 
-  this.filterLocations = ko.computed(function () {
+  this.filterLocations = ko.computed(function() {
     var ans = [];
     for (var i = 0; i < self.locations().length; i++) {
       var currLoc = self.locations()[i];
-      if (currLoc.name.toLowerCase().indexOf(self.filterText().toLowerCase()) !== -1) {
+      if (
+        currLoc.name.toLowerCase().indexOf(self.filterText().toLowerCase()) !==
+        -1
+      ) {
         currLoc.visibleMarker(true);
         ans.push(currLoc);
       } else {
@@ -103,31 +99,30 @@ var viewModel = function (data) {
     return ans;
   });
 
-  this.showItemInfo = function (e) {
+  this.showItemInfo = function(e) {
     infoWindow.setContent(e.infoWindowContent);
     infoWindow.open(map, e.marker);
     toggleBounce(e.marker);
 
     map.panTo(e.marker.getPosition());
   };
-
-
-
-}
-
+};
 
 function setupMarkers() {
   var bounds = new google.maps.LatLngBounds();
-  viewModel.locations().forEach(function (currentLocation) {
+  viewModel.locations().forEach(function(currentLocation) {
     bounds.extend(currentLocation.marker.position);
     currentLocation.marker.setMap(map);
-    currentLocation.marker.addListener('click', function (currentLocation) {
-      return function () {
-        infoWindow.setContent(currentLocation.infoWindowContent);
-        infoWindow.open(map, currentLocation.marker);
-      }
-    }(currentLocation));
-
+    currentLocation.marker.addListener(
+      "click",
+      (function(currentLocation) {
+        return function() {
+          infoWindow.setContent(currentLocation.infoWindowContent);
+          infoWindow.open(map, currentLocation.marker);
+          toggleBounce(currentLocation.marker);
+        };
+      })(currentLocation)
+    );
   });
 
   map.fitBounds(bounds);
@@ -142,37 +137,55 @@ function GooglemapLoadingError() {
   );
 }
 
-
 function createInfoWindow(location) {
+  var url_href =
+    '<a href="' +
+    location.url +
+    '"><i class="fa fa-home"></i> ' +
+    location.url +
+    "</a><br />";
+  var url_html =
+    location.url !== undefined
+      ? url_href
+      : '<i class="fa fa-home"></i> No website found.<br />';
 
-  var url_href = '<a href="' + location.url + '"><i class="fa fa-home"></i> ' + location.url + '</a><br />';
-  var url_html = location.url !== undefined ? url_href : '<i class="fa fa-home"></i> No website found.<br />';
+  var phone_href =
+    '<a href="tel:' +
+    location.phone +
+    '"><i class="fa fa-phone"></i>' +
+    location.phone +
+    "</a><br />";
+  var phone_html =
+    location.phone !== undefined
+      ? phone_href
+      : '<i class="fa fa-phone"></i> No phone number(s) found.<br />';
 
-  var phone_href = '<a href="tel:' + location.phone + '"><i class="fa fa-phone"></i>' + location.phone + '</a><br />';
-  var phone_html = location.phone !== undefined ? phone_href : '<i class="fa fa-phone"></i> No phone number(s) found.<br />';
-
-  return '<div id="info-window-content">' +
-    '<h3 id="firstHeading" class="firstHeading">' + location.name + '</h3>' +
-    '<i class="fa fa-location-arrow"></i> ' + location.address() + '<br />' +
-    phone_html + url_html + '</div>';
+  return (
+    '<div id="info-window-content">' +
+    '<h3 id="firstHeading" class="firstHeading">' +
+    location.name +
+    "</h3>" +
+    '<i class="fa fa-location-arrow"></i> ' +
+    location.address() +
+    "<br />" +
+    phone_html +
+    url_html +
+    "</div>"
+  );
 }
 
-
-
 function changeMap() {
-  if (map === null)
-    return;
+  if (map === null) return;
 
   var bounds = new google.maps.LatLngBounds();
-  viewModel.locations().forEach(function (location) {
+  viewModel.locations().forEach(function(location) {
     location.marker.setVisible(location.visibleMarker());
     bounds.extend(location.marker.position);
   });
   map.fitBounds(bounds);
 }
 
-
-$(document).ready(function () {
+$(document).ready(function() {
   // run test on initial page load
   checkSize();
 
@@ -182,13 +195,22 @@ $(document).ready(function () {
 
 //Function to the css rule
 function checkSize() {
-  if ($('nav').css('display') === 'none') {
-    $('.main-container').removeClass('sidebar_shown');
-    $('.sidebar').removeClass('sidebar_shown');
+  if ($("nav").css("display") === "none") {
+    $(".main-container").removeClass("sidebar_shown");
+    $(".sidebar").removeClass("sidebar_shown");
   }
 }
 
-
+function toggleBounce(marker) {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function() {
+      marker.setAnimation(null);
+    }, 1400);
+  }
+}
 
 /*
  * @description The Starting Point
@@ -205,11 +227,7 @@ function startApp() {
   infoWindow = new google.maps.InfoWindow();
   viewModel.getLocations(FSQ_URL);
 
-  viewModel.filterLocations.subscribe(function () {
+  viewModel.filterLocations.subscribe(function() {
     changeMap();
   });
 }
-
-
-
-
